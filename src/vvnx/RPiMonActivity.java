b377db25b6bdb4ec5028fadf6cade7e2437c5c3c
dnagis <vincent.achard@gmail.com>
2022-03-26 15:11:01 +0100

    amélioration pipeline rpicamsrc

--- src/vvnx/RPiMonActivity.java
+++ src/vvnx/RPiMonActivity.java
@@ -17,7 +17,9 @@ STDIN=$(cat) #Recuperation du message envoye par l emetteur
 case "$STDIN" in
  start)
     kill `pidof test-launch`
-    gst-launch-1.0 -e rpicamsrc bitrate=1000000 ! h264parse ! mp4mux ! filesink location=/root/capture.mp4
+    #il faut mettre la paht complete pour filesink
+    gst-launch-1.0 -e rpicamsrc bitrate=500000 annotation-mode=0x00000008 annotation-text-colour=0x000000 vflip=true hflip=true ! \
+ 'video/x-h264, width=640, height=480, profile=high' ! h264parse ! mp4mux ! filesink location=/root/capture.mp4
     ;;
  stop)
     kill -s SIGINT `pidof gst-launch-1.0`
