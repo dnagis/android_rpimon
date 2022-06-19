409ca371353fa0e5a51e631976d13c52358268fa
dnagis <vincent.achard@gmail.com>
2022-06-19 16:59:43 +0200

    camHQ

--- bash/gst_respawn.sh
+++ bash/gst_respawn.sh
@@ -8,8 +8,12 @@
 #gst-play-1.0 --videosink vaapisink out.mp4
 
 pipeline_current () {
-gst-launch-1.0 rpicamsrc bitrate=500000 annotation-mode=0x00000008 annotation-text-colour=0x000000 vflip=true hflip=true ! \
-'video/x-h264, width=640, height=480, profile=high' ! h264parse ! mpegtsmux ! filesink location=/root/capture-`date '+%H-%M-%S'`.ts	
+#camHQ
+gst-launch-1.0 rpicamsrc bitrate=1000000 ! \
+'video/x-h264, width=1280, height=720, profile=high' ! h264parse ! mpegtsmux ! filesink location=/root/capture-`date '+%H-%M-%S'`.ts
+#cam v2
+#gst-launch-1.0 rpicamsrc bitrate=500000 annotation-mode=0x00000008 annotation-text-colour=0x000000 vflip=true hflip=true ! \
+#'video/x-h264, width=640, height=480, profile=high' ! h264parse ! mpegtsmux ! filesink location=/root/capture-`date '+%H-%M-%S'`.ts	
 }
 
 #systeme de respawn
