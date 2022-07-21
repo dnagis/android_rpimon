05906bc7ea8ca621fe68cb856788b7a6afbe8f45
dnagis <vincent.achard@gmail.com>
2022-07-21 10:02:06 +0200

    tee filsink + tcpserver

--- bash/gst_respawn.sh
+++ bash/gst_respawn.sh
@@ -3,17 +3,21 @@
 #lancement gst-launch-1.0 wrappe dans une enveloppe de respawn crash-proof
 
 #ts --> mp4
-#gst-launch-1.0 filesrc location=`ls capture*.ts` ! tsdemux ! h264parse ! mp4mux ! filesink location=out.mp4
+#gst-launch-1.0 filesrc location=capture.ts ! tsdemux ! h264parse ! mp4mux ! filesink location=out.mp4
 #
 #gst-play-1.0 --videosink vaapisink out.mp4
 
 pipeline_current () {
-#camHQ
-gst-launch-1.0 rpicamsrc bitrate=1000000 ! \
-'video/x-h264, width=1280, height=720, profile=high' ! h264parse ! mpegtsmux ! filesink location=/root/capture-`date '+%H-%M-%S'`.ts
-#cam v2
+#RPiCam -> tee -> filesink + tcpserversink
+gst-launch-1.0 v4l2src do-timestamp=true ! video/x-h264,width=640,height=480,framerate=30/1 ! h264parse ! tee name=t \
+t. ! queue ! mpegtsmux ! filesink location=/root/capture.ts \
+t. ! queue ! rtph264pay config-interval=1 ! gdppay ! tcpserversink port=8888 host=0.0.0.0
+#camV2
 #gst-launch-1.0 rpicamsrc bitrate=500000 annotation-mode=0x00000008 annotation-text-colour=0x000000 vflip=true hflip=true ! \
 #'video/x-h264, width=640, height=480, profile=high' ! h264parse ! mpegtsmux ! filesink location=/root/capture-`date '+%H-%M-%S'`.ts	
+#camHQ
+#gst-launch-1.0 rpicamsrc bitrate=1000000 ! \
+#'video/x-h264, width=1280, height=720, profile=high' ! h264parse ! mpegtsmux ! filesink location=/root/capture-`date '+%H-%M-%S'`.ts
 }
 
 #systeme de respawn
